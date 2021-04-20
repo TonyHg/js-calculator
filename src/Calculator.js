197c0302357418e45ad8bc4ff06618556c6f5b8b
Tony HENG <tony.heng@epita.fr>
2021-04-20 19:26:35 +0200

    set constants

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,6 +3,23 @@ import './Calculator.css'
 import Screen from './Screen'
 import NumPad from './NumPad'
 
+const MAXDIGITS = 13;
+export const AC="AC", 
+            C="C",
+            ZERO="0",
+            PLUS="+",
+            MINUS="-",
+            TIMES="*",
+            DIVIDE="/",
+            MODULO="%",
+            DECIMAL=".",
+            EQUAL="=";
+const initState = {
+    formula: "",
+    currentNumber: ZERO,
+    evaluating: false
+}
+
 export default class Calculator extends React.Component {
     constructor(props) {
         super(props);
