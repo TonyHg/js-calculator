7c905dca1c9fecb3908509e64b3a1e98b273d322
Tony HENG <tony.heng@epita.fr>
2021-04-20 19:32:53 +0200

    add credit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Calculator from './Calculator'
 
+const GITHUB_LINK = "https://github.com/TonyHg"
 function App() {
   return (
     <div id="wrapper">
       <Calculator />
+      <div id="credit">Tony Heng - <a href={GITHUB_LINK}>Github</a></div>
     </div>
   );
 }
